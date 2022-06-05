ea68244e16df79927617278fd2f912dd790e9de6
Guilherme <guissantana105@gmail.com>
2022-06-05 00:47:51 -0400

    create pages

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-1">
+        Qual o pokemon <br />
+        você escolheria?
+      </h1>
+      <h3 className="text-2">
+          Você pode saber o tipo de Pokémon, seus pontos <br/>
+          fortes, fracos e habilidades.
+      </h3>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar () {
+    return(
+        <div className="App">
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+          alt="#"
+        />
+        
+        </div>
+        )
+}
+
+export default Navbar;
