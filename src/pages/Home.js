d319ec6834c9c2851ae175f3bb1e5f3e8fd70a6c
Guilherme <guissantana105@gmail.com>
2022-06-05 14:38:49 -0400

    Style home inital

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,22 @@ import "../style/Home.css";
 
 function Home() {
   return (
+    
     <footer className="footer-container">
+      >
       <div className="container-text">
         <div className="text-1">
-        <h1>Qual o pokemon você escolheria?</h1>
-      </div>
-      <div className="text-2">
-        <h3>
+          <h1>Qual o pokemon você escolheria?</h1>
+        </div>
+        <div className="text-2">
           Você pode saber o tipo de Pokémon, seus pontos fortes, fracos e
           habilidades.
-        </h3>
-      </div>
         </div>
+        
+      </div>
+      <div className="container-fundo">
+        <img className="img-fundo" src="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Pikachu-Pok%C3%A9mon-PNG-983x1024.png" alt="pikachu.fundo"/>
+      </div>
     </footer>
   );
 }
